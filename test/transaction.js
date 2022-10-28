5deb1d42894893d9fe4a21785e437109f7cd8e39
Evgeny Vlasenko <mahnunchik@gmail.com>
2022-10-28 18:35:00 +0400

    feat: estimate transaction size

--- test/transaction.js
+++ test/transaction.js
@@ -27,4 +27,14 @@ describe('TransactionCapsule', () => {
     // eslint-disable-next-line max-len
     assert.strictEqual(hex, '0acc010a02e6852208272beb1a42cfa07b40f8b2f0fca3305aae01081f12a9010a31747970652e676f6f676c65617069732e636f6d2f70726f746f636f6c2e54726967676572536d617274436f6e747261637412740a1541cd2a3d9f938e13cd947ec05abc7fe734df8dd826121541ea51342dabbb928ae1e576bd39eff8aaf070a8c62244a9059cbb0000000000000000000000000c2bb55b3acd8b6351b31ff211ee0f484688e5da000000000000000000000000000000000000000000000000000000000000002a900180ade20412414ec770c53f3521bf4f8a41a5deeb540419e981fe41acc0014caf883108dc5b77998bc831a9ee52dda8b1407ee4028d8c7641f680e592ec65fb31dde686abd80901');
   });
+
+  it('should estimate TRX transaction size', () => {
+    const size = TransactionCapsule.estimateTransferSize(42, 1100000);
+    assert.strictEqual(size, 263);
+  });
+
+  it('should estimate Token transaction size', () => {
+    const size = TransactionCapsule.estimateTransferTokenSize(42, 10000000);
+    assert.strictEqual(size, 338);
+  });
 });
